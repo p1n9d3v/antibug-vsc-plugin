e70507da434387bb9aa4bbe9f31d7dc7749df892
bl0ckp1n9 <blockp1n9@gmail.com>
2023-10-28 00:14:47 +0900

    feat: deploy initial 파일 및 html 구조

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import CompileViewProvider from "./provider/compile";
+import DeployViewProvider from "./provider/deploy";
 
 export function activate(context: vscode.ExtensionContext) {
   const compileViewProvider = new CompileViewProvider({
@@ -7,10 +8,19 @@ export function activate(context: vscode.ExtensionContext) {
     viewType: "compile",
   });
 
+  const deployViewProvider = new DeployViewProvider({
+    extensionUri: context.extensionUri,
+    viewType: "deploy",
+  });
+
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(
       "antiblock.compile",
       compileViewProvider
+    ),
+    vscode.window.registerWebviewViewProvider(
+      "antiblock.deploy",
+      deployViewProvider
     )
   );
 }
