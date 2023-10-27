aa57500ce4e7ce66fec0c88e40f2bc33d2093551
bl0ckp1n9 <blockp1n9@gmail.com>
2023-10-27 19:39:29 +0900

    feat: webview provider 클래스 생성

---
+++ src/provider/webview.ts
@@ -0,0 +1,48 @@
+import * as vscode from "vscode";
+
+export class WebviewProvider implements vscode.WebviewViewPropvider {
+  public extensionUri: vscode.Uri;
+  public viewType: string;
+  public commonFiles: Map<string, vscode.Uri[]> = new Map();
+  constructor({
+    extensionUri,
+    viewType,
+  }: {
+    extensionUri: vscode.Uri;
+    viewType: string;
+  }) {
+    this.extensionUri = extensionUri;
+    this.viewType = viewType;
+
+    this.commonFiles.set("style", [
+      vscode.Uri.joinPath(
+        this.extensionUri,
+        "src",
+        "style",
+        "common",
+        "reset.css"
+      ),
+      vscode.Uri.joinPath(
+        this.extensionUri,
+        "src",
+        "style",
+        "common",
+        "global.css"
+      ),
+    ]);
+
+    this.commonFiles.set("template", [
+      vscode.Uri.joinPath(this.extensionUri, "src", "template"),
+    ]);
+  }
+
+  protected getNonce() {
+    let text = "";
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+}
